08396d050b5b3e7960a86caef3a75c1b2fa792d7
Tom Widdows <thomas.widdows10@gmail.com>
2024-07-02 07:25:12 +0000

    Add expand.html page and add routing decorator for it in run.py. Add links to navbar using jinja template method 'url_for'.

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 # Create instance of the Flask class
 app = Flask(__name__)
 
 # Route decorator targetting root directory
 @app.route("/")
-# filler function
 def index():
     return render_template("index.html")
+
+# Route decorator targetting expand.html page
+@app.route("/expand")
+def expand():
+    return render_template("expand.html")
 
 # Run app if the default module is chosen
 if __name__ == "__main__":
