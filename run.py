6eaf754bdd592ce4883a365b4071bf3378c54279
Tom Widdows <thomas.widdows10@gmail.com>
2024-07-02 07:08:35 +0000

    Create index.html and add boilerplate. render index.html template within route to root directory

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import os
 from flask import Flask
 
+# Create instance of the Flask class
 app = Flask(__name__)
 
+# Route decorator targetting root directory
 @app.route("/")
+# filler function
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
-
+# Run app if the default module is chosen
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
