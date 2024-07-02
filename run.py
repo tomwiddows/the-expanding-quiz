b9eab67a363fb8254e37f01720a6682a5ff28fc4
Tom Widdows <thomas.widdows10@gmail.com>
2024-07-02 08:37:58 +0000

    Add double spacing between functions in run.py. Create static folder with style.css within. Add style.css link to base.html and add h1 elemend to body before block template begins. Change placeholder e text in index and expand.html to h2 element

--- run.py
+++ run.py
@@ -4,15 +4,18 @@ from flask import Flask, render_template
 # Create instance of the Flask class
 app = Flask(__name__)
 
+
 # Route decorator targetting root directory
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 # Route decorator targetting expand.html page
 @app.route("/expand")
 def expand():
     return render_template("expand.html")
+
 
 # Run app if the default module is chosen
 if __name__ == "__main__":
