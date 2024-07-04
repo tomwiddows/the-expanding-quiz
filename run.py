c61296a94cbae1584da640f913513f2f5ff54ba5
Tom Widdows <thomas.widdows10@gmail.com>
2024-07-04 10:47:12 +0000

    connect to mongo database

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+uri = "mongodb+srv://TomW:97VVD88Rob94Hav@usersandquestions.ochexzh.mongodb.net/?retryWrites=true&w=majority&appName=usersAndQuestions"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 # Create instance of the Flask class
 app = Flask(__name__)
@@ -20,6 +39,24 @@ def expand():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        #Check if username already exists in db
+        existing_user = mongo.db.user.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
