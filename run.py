752cb48d4e066daaf01b79f09d0f3ece78812636
Tom Widdows <thomas.widdows10@gmail.com>
2024-07-18 18:01:17 +0000

    Set debug to false

--- run.py
+++ run.py
@@ -276,4 +276,4 @@ if __name__ == '__main__':
     app.run(
         host=os.environ.get('IP', '0.0.0.0'),
         port=int(os.environ.get('PORT', '5000')),
-        debug=True)  # Set to False in production
+        debug=False)  # Set to False in production
